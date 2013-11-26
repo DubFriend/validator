df3556c01078d2226c8b66b10a3a8d2f253c6325
Brian Detering <bdeterin@gmail.com>
2013-11-25 16:09:58 -0800

    basic php implemntation complete

--- validator.php
+++ validator.php
@@ -10,19 +10,23 @@ class ValidatorTestWrapper {
         $this->test = $test;
     }
 
+    //message if test fails
     function message() {
         return array_values($this->test)[0];
     }
 
-    //'name:value'
+    // just 'name:value' stuck together
     function description() {
         return array_keys($this->test)[0];
     }
 
+    //name of the test (required, regex, etc.)
     function name() {
         return explode(':', $this->description())[0];
     }
 
+    //value associated with test, (regex:/abc/) (not the value of the
+    //data to be tested)
     function value() {
         $pieces = explode(':', $this->description());
         return count($pieces) > 1 ? $pieces[1] : null;
@@ -32,10 +36,7 @@ class ValidatorTestWrapper {
 class Validator {
     private $schema;
     function __construct($schema) {
-        $this->schema = $this->wrapSchema(
-            is_array($schema) ?
-                $schema : json_decode(file_get_contents($schema), true)
-        );
+        $this->schema = $this->wrapSchema($schema);
     }
 
     private function wrapSchema($schema) {
@@ -49,17 +50,47 @@ class Validator {
         return $wrapped;
     }
 
-    private function fetch($key, array $array) {
-        return array_key_exists($key, $array) ? $array[$key] : null;
-    }
-
     private function isTestPass($valueToTest, $testObject) {
-        return $this->{$testObject->name()}($valueToTest, $testObject->value());
+        $testSymbolMap = array(
+            '==' => 'equalTo',
+            '<' => 'lessThan',
+            '<=' => 'lessThanOrEqualTo',
+            '>' => 'greaterThan',
+            '>=' => 'greaterThanOrEqualTo'
+        );
+
+        $methodName = in_array($testObject->name(), array_keys($testSymbolMap)) ?
+                $testSymbolMap[$testObject->name()] :
+                $testObject->name();
+
+        return $this->{$methodName}($valueToTest, $testObject->value());
     }
 
     // TEST FUNCTIONS START
-    private function required($valueToTest, $testValue) {
+    private function required($valueToTest) {
         return $valueToTest ? true : false;
+    }
+
+    private function type($valueToTest, $testValue) {
+        $type = gettype($valueToTest);
+        switch($testValue) {
+            case 'number':
+                return $type === 'integer' || $type === 'double';
+            case 'string':
+                return $type === 'string';
+            case 'boolean':
+                return $type === 'boolean';
+            case 'object':
+                return $type === 'array';
+            case 'null':
+                return $type === 'NULL';
+            default:
+                throw new ValidatorException('invalid type ' . $valueToTest);
+        }
+    }
+
+    private function email($valueToTest) {
+        return filter_var($valueToTest, FILTER_VALIDATE_EMAIL);
     }
 
     private function minimumLength($valueToTest, $testValue) {
@@ -81,19 +112,50 @@ class Validator {
             return $result === 1;
         }
     }
+
+    private function lessThan($valueToTest, $testValue) {
+        return $valueToTest < $testValue;
+    }
+
+    private function lessThanOrEqualTo($valueToTest, $testValue) {
+        return $valueToTest <= $testValue;
+    }
+
+    private function greaterThan($valueToTest, $testValue) {
+        return $valueToTest > $testValue;
+    }
+
+    private function greaterThanOrEqualTo($valueToTest, $testValue) {
+        return $valueToTest >= $testValue;
+    }
+
+    private function equalTo($valueToTest, $testValue) {
+        return $valueToTest == $testValue;
+    }
+
+    private function match($valueToTest) {
+        if(!is_array($valueToTest) || count($valueToTest) !== 2) {
+            throw new ValidatorException('match must recieve an array with two values');
+        }
+        else {
+            return $valueToTest[0] == $valueToTest[1];
+        }
+    }
     // TEST FUNCTIONS END
 
     function test(array $dataToTest) {
         $errors = array();
         foreach($this->schema as $name => $tests) {
-            $valueToTest = $this->fetch($name, $dataToTest);
-            if($tests[0]->name() === 'required' || $valueToTest) {
+            if(array_key_exists($name, $dataToTest)) {
                 foreach($tests as $testObject) {
-                    if(!$this->isTestPass($valueToTest, $testObject)) {
+                    if(!$this->isTestPass($dataToTest[$name], $testObject)) {
                         $errors[$name] = $testObject->message();
                         break;
                     }
                 }
+            }
+            else if($tests[0]->name() === 'required') {
+                $errors[$name] = $tests[0]->message();
             }
         }
         return $errors;
--- validatorTest.php
+++ validatorTest.php
@@ -4,7 +4,15 @@ require_once 'validator.php';
 class ValidateTest extends PHPUnit_Framework_TestCase {
 	private $validator;
 	function setUp() {
-		$this->validator = new Validator('signup.example.json');
+		$this->validator = new Validator(
+			json_decode(file_get_contents('signup.example.json'), true)
+		);
+	}
+
+	private function validateExtended(array $data = array()) {
+		return $this->validator->test(
+			array_merge(array('username' => 'pass'), $data)
+		);
 	}
 
 	function testUsernamePass() {
@@ -13,11 +21,14 @@ class ValidateTest extends PHPUnit_Framework_TestCase {
 		);
 	}
 
-	function testFailRequired() {
+	function testFailRequiredFalsyValue() {
 		$this->assertEquals(
 			array('username' => 'username required'),
 			$this->validator->test(array('username' => ''))
 		);
+	}
+
+	function testFailRequiredKeyNotPresent() {
 		$this->assertEquals(
 			array('username' => 'username required'),
 			$this->validator->test(array())
@@ -62,6 +73,177 @@ class ValidateTest extends PHPUnit_Framework_TestCase {
 			array('username' => 'alphanumeric only'),
 			$this->validator->test(array('username' => 'f@il'))
 		);
+	}
+
+	function testTypeNumberPass() {
+		$this->assertEquals(
+			array(),
+			$this->validateExtended(array('number' => 5))
+		);
+	}
+
+	function testTypeNumberFail() {
+		$this->assertEquals(
+			array('number' => 'must be of type number'),
+			$this->validateExtended(array('number' => '5'))
+		);
+	}
+
+	function testTypeStringPass() {
+		$this->assertEquals(
+			array(), $this->validateExtended(array('string' => 'foo'))
+		);
+	}
+
+	function testTypeStringFail() {
+		$this->assertEquals(
+			array('string' => 'must be of type string'),
+			$this->validateExtended(array('string' => true))
+		);
+	}
+
+	function testTypeBooleanPass() {
+		$this->assertEquals(
+			array(), $this->validateExtended(array('boolean' => false))
+		);
+	}
+
+	function testTypeBooleanFailZero() {
+		$this->assertEquals(
+			array('boolean' => 'must be of type boolean'),
+			$this->validateExtended(array('boolean' => 0))
+		);
+	}
+
+	function testTypeBooleanFailNull() {
+		$this->assertEquals(
+			array('boolean' => 'must be of type boolean'),
+			$this->validateExtended(array('boolean' => null))
+		);
+	}
+
+	function testTypeObjectPass() {
+		$this->assertEquals(
+			array(), $this->validateExtended(array('object' => array()))
+		);
+	}
+
+	function testTypeObjectFail() {
+		$this->assertEquals(
+			array('object' => 'must be of type object'),
+			$this->validateExtended(array('object' => null))
+		);
+	}
+
+	function testTypeNullPass() {
+		$this->assertEquals(
+			array(), $this->validateExtended(array('null' => null))
+		);
+	}
+
+	function testTypeNullFail() {
+		$this->assertEquals(
+			array('null' => 'must be of type null'),
+			$this->validateExtended(array('null' => ''))
+		);
+	}
+
+	function testLessThanPass() {
+		$this->assertEquals(
+			array(), $this->validateExtended(array('lessThan' => -1))
+		);
+	}
+
+	function testLessThanFail() {
+		$this->assertEquals(
+			array('lessThan' => 'must be less than zero'),
+			$this->validateExtended(array('lessThan' => 0))
+		);
+	}
+
+	function testLessThanOrEqualToPass() {
+		$this->assertEquals(
+			array(), $this->validateExtended(array('lessThanOrEqualTo' => 0))
+		);
+	}
+
+	function testLessThanOrEqualToFail() {
+		$this->assertEquals(
+			array('lessThanOrEqualTo' => 'must be less than or equal to zero'),
+			$this->validateExtended(array('lessThanOrEqualTo' => 0.1))
+		);
+	}
+
+	function testGreaterThanPass() {
+		$this->assertEquals(
+			array(), $this->validateExtended(array('greaterThan' => 1))
+		);
+	}
+
+	function testGreaterThanFail() {
+		$this->assertEquals(
+			array('greaterThan' => 'must be greater than zero'),
+			$this->validateExtended(array('greaterThan' => 0))
+		);
+	}
+
+	function testGreaterThanOrEqualToPass() {
+		$this->assertEquals(
+			array(), $this->validateExtended(array('greaterThanOrEqualTo' => 0))
+		);
+	}
+
+	function testGreaterThanOrEqualToFail() {
+		$this->assertEquals(
+			array('greaterThanOrEqualTo' => 'must be greater than or equal to zero'),
+			$this->validateExtended(array('greaterThanOrEqualTo' => -0.1))
+		);
+	}
+
+	function testEqualToPass() {
+		$this->assertEquals(
+			array(), $this->validateExtended(array('equalTo' => '0'))
+		);
+	}
+
+	function testEqualToFail() {
+		$this->assertEquals(
+			array('equalTo' => 'must be equal to zero'),
+			$this->validateExtended(array('equalTo' => 1))
+		);
+	}
+
+	function testEmailPass() {
+		$this->assertEquals(
+			array(), $this->validateExtended(array('email' => 'mail@mail.com'))
+		);
+	}
+
+	function testEmailFail() {
+		$this->assertEquals(
+			array('email' => 'bad email format'),
+			$this->validateExtended(array('email' => 'wrong'))
+		);
+	}
+
+	function testMatchPass() {
+		$this->assertEquals(
+			array(), $this->validateExtended(array('match' => array('10', 10)))
+		);
+	}
+
+	function testMatchFail() {
+		$this->assertEquals(
+			array('match' => 'values must match'),
+			$this->validateExtended(array('match' => array(1, 2)))
+		);
+	}
+
+	/**
+     * @expectedException ValidatorException
+     */
+	function testMatchInproperFormat() {
+		$this->validateExtended(array('match' => array(1)));
 	}
 }
 ?>
